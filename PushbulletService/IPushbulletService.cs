df49cd159cbbc1db8575b7127cf7f1da77969832
Alvin Jorrel Pascual <pascual.alvin@gmail.com>
2016-04-27 10:37:31 -0700

    Initial commit of Pushbullet Class Library for .NET

---
+++ PushbulletService/IPushbulletService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushbulletService
+{
+    public interface IPushbulletService
+    {
+        void PbPush(string title, string message, string url, string apiKey);
+    }
+}
---
+++ PushbulletService/PushbulletService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushbulletService
+{
+    public class PushbulletService : IPushbulletService
+    {
+        /// <summary>
+        /// Sends a Pushbullet push notification to a specific user, identified by API key.
+        /// Notification can either be a text or include a link.
+        /// </summary>
+        /// <param name="title">Notification title</param>
+        /// <param name="message">Notification body</param>
+        /// <param name="url">Add a URL to push, leave empty for a note push</param>
+        /// <param name="apiKey">API key required to send push to correct user</param>
+        public void PbPush(string title, string message, string url, string apiKey)
+        {
+            WebRequest request = WebRequest.Create("https://api.pushbullet.com/v2/pushes");
+
+            request.Method = "POST";
+            request.Headers.Add("Authorization", "Bearer " + apiKey);
+            request.ContentType = "application/json; charset=UTF-8";
+            string requestString = "{\"body\":\"" + message + "\", \"title\":\"" + title + "\", \"type\":" 
+                + (!string.IsNullOrEmpty(url) ? ("\"link\", \"url\":\"" + url + "\"") : "\"note\"}");
+            byte[] buffer = Encoding.GetEncoding("UTF-8").GetBytes(requestString);
+            request.ContentLength = buffer.Length;
+
+            Stream stream = request.GetRequestStream();
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Close();
+
+            WebResponse response = request.GetResponse();
+        }
+    }
+}
